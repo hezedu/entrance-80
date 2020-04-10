1a989a885ec3fc1335a574b4d37cc171ecb18cc2
tianxiazhizhong <tianxiazhizhong@gmail.com>
2020-04-10 10:45:06 +0800

    up

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # entrance-80
+https://github.com/linux-remote/linux-remote/wiki/letsencrypt-SSL-%E8%AF%81%E4%B9%A6%E7%94%9F%E6%88%90
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  certbotWebRoot: '/opt/entrance-80/static/',
+  certbotWebRoot: '/opt/entrance-80/certbot-web-root/',
   domain: 'demo.linux-remote.org'
 }
